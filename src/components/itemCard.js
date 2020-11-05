4d0e8c4a596db65462372c07895add5122926c45
Zett-8 <kame0008@gmail.com>
2020-11-05 21:16:11 +0100

    feature vertical list dnd

--- src/components/itemCard.js
+++ src/components/itemCard.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const ItemCard = props => {
   const provided = props.provided
   return (
-    <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+    <Card {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
       {props.children}
-    </div>
+    </Card>
   )
 }
+
+const Card = styled.div`
+  display: grid;
+  justify-content: center;
+  align-content: center;
+
+  background-color: #eee;
+  height: 60px;
+  width: 98%;
+
+  margin-bottom: 9px;
+`
 
 export default ItemCard
--- src/components/verticalList.js
+++ src/components/verticalList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
 import ItemCard from './itemCard'
 
 const getItem = n => Array.from({ length: n }).map((v, i) => ({ id: i, content: `content-${i}` }))
@@ -11,26 +13,42 @@ const verticalList = () => {
     setItem(getItem(10))
   }, [])
 
-  const onDragEnd = re => {
-    if (!re.destination) return null
+  const onDragEnd = result => {
+    if (!result.destination) return null
+    setItem(reorder(result.source.index, result.destination.index))
   }
 
-  console.log(item)
+  const reorder = (source, destination) => {
+    const newItem = item.slice(0)
+    const [x] = newItem.splice(source, 1)
+    newItem.splice(destination, 0, x)
+    return newItem
+  }
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Droppable droppableId="droppable">
         {provided => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
+          <CardList {...provided.droppableProps} ref={provided.innerRef}>
             {item.map((v, i) => (
-              <Draggable key={v.id} draggableId={v.id + ''} index={i}>
+              <Draggable key={v.id} draggableId={String(v.id)} index={i}>
                 {(provided, snapshot) => <ItemCard provided={provided}>{v.content}</ItemCard>}
               </Draggable>
             ))}
-          </div>
+            {provided.placeholder}
+          </CardList>
         )}
       </Droppable>
     </DragDropContext>
   )
 }
+
+const CardList = styled.div`
+  display: grid;
+  justify-items: center;
+
+  width: 300px;
+  border: 1px black solid;
+`
 
 export default verticalList
